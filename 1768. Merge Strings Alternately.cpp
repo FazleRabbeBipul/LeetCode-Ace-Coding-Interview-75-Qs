f07636a9ec4f2ed0c723adc19f9d623a754621bb
FazleRabbeBipul <rabbecse1677@gmail.com>
2024-01-16 18:26:48 +0600

    Array/String

---
+++ 1768. Merge Strings Alternately.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    string mergeAlternately(string s1, string s2)
+    {
+        string s = "";
+        int i = 0, n = s1.size(), m = s2.size();
+
+        while (i < n || i < m)
+        {
+            if (i < n)
+                s += s1[i];
+            if (i < m)
+                s += s2[i];
+            i++;
+        }
+
+        return s;
+    }
+};
